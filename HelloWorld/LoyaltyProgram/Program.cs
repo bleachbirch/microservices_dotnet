6d4c5df6fb868455cea93ff7070fd44ef8cbf22c
Anastasiya Matveeva <bonnbonnbonn.bn@gmail.com>
2021-11-27 13:31:32 +0300

    chapter 4: defined project structure

--- HelloWorld/LoyaltyProgram/Program.cs
+++ HelloWorld/LoyaltyProgram/Program.cs
@@ -1,6 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Nancy.Owin;
 
-app.MapGet("/", () => "Hello World!");
+var app = ConfigureBuilder().Build();
+
+app.UseOwin(pipeline => pipeline.UseNancy());
 
 app.Run();
+
+WebApplicationBuilder ConfigureBuilder()
+{
+    var builder = WebApplication.CreateBuilder();
+
+    builder.Services.Configure<KestrelServerOptions>(config =>
+    {
+        config.AllowSynchronousIO = true;
+    });
+
+    return builder;
+}
