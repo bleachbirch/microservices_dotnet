33e52f4723994031887b2ec0449c807d39659b84
Anastasiya Matveeva <bonnbonnbonn.bn@gmail.com>
2021-11-27 13:40:59 +0300

    chapter 4: configured nancy bootstrapper

---
+++ HelloWorld/LoyaltyProgram/Bootstrapper.cs
@@ -0,0 +1,11 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+
+namespace LoyaltyProgram
+{
+    public class Bootstrapper: DefaultNancyBootstrapper
+    {
+        protected override Func<ITypeCatalog, NancyInternalConfiguration> InternalConfiguration => 
+            NancyInternalConfiguration.WithOverrides(builder => builder.StatusCodeHandlers.Clear());
+    }
+}
