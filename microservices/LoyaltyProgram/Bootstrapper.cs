ea6810d3254a44dac511d0937b9a68cfdae80c9a
Anastasiya Matveeva <bonnbonnbonn.bn@gmail.com>
2021-12-05 18:02:18 +0300

    chapter 6: logging unhandled exceptions

--- microservices/LoyaltyProgram/Bootstrapper.cs
+++ microservices/LoyaltyProgram/Bootstrapper.cs
@@ -1,5 +1,6 @@
 ﻿using Nancy;
 using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
 
 namespace LoyaltyProgram
 {
@@ -7,5 +8,19 @@ namespace LoyaltyProgram
     {
         protected override Func<ITypeCatalog, NancyInternalConfiguration> InternalConfiguration => 
             NancyInternalConfiguration.WithOverrides(builder => builder.StatusCodeHandlers.Clear());
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            pipelines.OnError += (ctx, ex) =>
+            {
+                Log("Unhandled", ex);
+                return null;
+            };
+        }
+
+        private void Log(string message, Exception ex)
+        {
+            //Отправляем параметры в централизованное хранилище журналов
+        }
     }
 }
