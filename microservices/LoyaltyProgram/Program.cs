a6354c7fb31ec8e29879cf89f1e49fe2d71aa698
Anastasiya Matveeva <bonnbonnbonn.bn@gmail.com>
2022-01-07 16:41:30 +0300

    chapter 10: ended implementation authorization in LoyaltyProgram

--- microservices/LoyaltyProgram/Program.cs
+++ microservices/LoyaltyProgram/Program.cs
@@ -1,9 +1,26 @@
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Owin;
 using Nancy.Owin;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 var app = ConfigureBuilder().Build();
 
-app.UseOwin(pipeline => pipeline.UseNancy());
+app.UseOwin(pipeline => 
+{
+    pipeline(next => env =>
+    {
+        var context = new OwinContext(env);
+        var principal = context.Request.User as ClaimsPrincipal;
+        if (principal is not null && principal.HasClaim("scope", "loyalty_program_write"))
+        {
+            return next(env);
+        }
+        context.Response.StatusCode = 403;
+        return Task.FromResult(0);
+    });
+    pipeline.UseNancy(); 
+});
 
 app.Run();
 
@@ -16,5 +33,15 @@ WebApplicationBuilder ConfigureBuilder()
         config.AllowSynchronousIO = true;
     });
 
+    JwtSecurityTokenHandler.DefaultInboundClaimTypeMap = new Dictionary<string, string>();
+    builder.Services
+        .AddAuthentication()
+        // Меняем настройки ASP.Net Core для чтения маркера из входящих запросов
+        .AddJwtBearer(options =>
+        {
+            options.Audience = "http://localhost:5001/resources";
+            options.Authority = "http://localhost:5001";
+            options.RequireHttpsMetadata = false;
+        });
     return builder;
 }
