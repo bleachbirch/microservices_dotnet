cd2647ca346fae6c5fe35e42308c68e80e283da4
Anastasiya Matveeva <bonnbonnbonn.bn@gmail.com>
2022-01-07 14:37:08 +0300

    chapter 10: added authentification to API Gateway

--- microservices/ApiGateway/Program.cs
+++ microservices/ApiGateway/Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Nancy.Owin;
+using System.IdentityModel.Tokens.Jwt;
 
 var app = ConfigureBuilder().Build();
 
-app.UseOwin(pipeline => pipeline.UseNancy());
+app.UseOwin(pipeline => pipeline.UseNancy())
+    .UseAuthentication();
 
 app.Run();
 
@@ -15,6 +18,26 @@ WebApplicationBuilder ConfigureBuilder()
     {
         config.AllowSynchronousIO = true;
     });
+
+    JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+    builder.Services
+        .AddAuthentication()
+        .AddCookie("Cookies");
+
+    var oidcOptions = new OpenIdConnectOptions
+    {
+        SignInScheme = "Cookies",
+        Authority = "http://localhost:5001",
+        RequireHttpsMetadata = false,
+        ClientId = "web",
+        ResponseType = "id_token token",
+        GetClaimsFromUserInfoEndpoint = true,
+        SaveTokens = true
+    };
+    oidcOptions.Scope.Clear();
+    oidcOptions.Scope.Add("openid");
+    oidcOptions.Scope.Add("profile");
+    oidcOptions.Scope.Add("api1");
 
     return builder;
 }
