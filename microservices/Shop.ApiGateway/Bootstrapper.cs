5f7bbbd410ed12b15d5f97a567db761207da9331
Anastasiya Matveeva <bonnbonnbonn.bn@gmail.com>
2022-01-24 08:45:06 +0300

    chapter 12: created api gateway

---
+++ microservices/Shop.ApiGateway/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using Microservice.Platform;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Configuration;
+using Nancy.TinyIoc;
+
+namespace Shop.ApiGateway
+{
+    internal class Bootstrapper : DefaultNancyBootstrapper
+    {
+        private Serilog.ILogger _logger;
+
+        public Bootstrapper(Serilog.ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            container.Register(_logger);
+            container.UseHttpClientFactory(new NancyContext());
+        }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            base.RequestStartup(container, pipelines, context);
+            container.UseHttpClientFactory(context);
+        }
+    }
+}
---
+++ microservices/Shop.ApiGateway/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Nancy.Owin;
+using Microservice.Logging;
+using Serilog;
+using Microservice.Platform;
+
+namespace Shop.ApiGateway
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            var logger = ConfigureLogger();
+            ConfigureMicroservicePlatform();
+            app.UseStaticFiles();
+            app.UseOwin()
+                .UseMonitoringAndLogging(logger, HealthCheck)
+                .UseNancy(opt => opt.Bootstrapper = new Bootstrapper(logger));
+        }
+
+        private void ConfigureMicroservicePlatform()
+        {
+            MicroservicePlatformHelper.Configure("http://localhost:5001/", "api_gateway", "secret");
+        }
+
+        private Task<bool> HealthCheck()
+        {
+            return Task.FromResult(true);
+        }
+
+        private Serilog.ILogger ConfigureLogger()
+        {
+            return new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.ColoredConsole(
+                Serilog.Events.LogEventLevel.Verbose,
+                "{NewLine}{TimeStamp:HH:mm:ss} [{Level}] ({CorrelationToken}) {Message}{NewLine}{Exception}")
+                .CreateLogger();
+        }
+    }
+}
